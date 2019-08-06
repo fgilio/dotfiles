9148f95671afece42be482e22a2e7a4534663431
Franco Gilio <franco.gilio@gmail.com>
2019-08-06 10:12:21 -0300

    wip

--- install.sh
+++ install.sh
@@ -17,29 +17,18 @@ brew bundle
 # Make ZSH the default shell environment
 chsh -s $(which zsh)
 
-# Install PHP extensions with PECL
-pecl install memcached imagick
+# # Install global Composer packages
+# /usr/local/bin/composer global require laravel/installer laravel/spark-installer laravel/valet
 
-# Install Composer
-curl -sS https://getcomposer.org/installer | php
-mv composer.phar /usr/local/bin/composer
+# # Install Laravel Valet
+# $HOME/.composer/vendor/bin/valet install
 
-# Install global Composer packages
-/usr/local/bin/composer global require laravel/installer laravel/spark-installer laravel/valet
+# # Install global NPM packages
+# yarn global
 
-# Install Laravel Valet
-$HOME/.composer/vendor/bin/valet install
-
-# Install global NPM packages
-npm install --global yarn
-
-# Create a Sites directory
-# This is a default directory for macOS user accounts but doesn't comes pre-installed
-mkdir $HOME/Sites
-
-# Removes .zshrc from $HOME (if it exists) and symlinks the .zshrc file from the .dotfiles
-rm -rf $HOME/.zshrc
-ln -s $HOME/.dotfiles/.zshrc $HOME/.zshrc
+# # Removes .zshrc from $HOME (if it exists) and symlinks the .zshrc file from the .dotfiles
+# rm -rf $HOME/.zshrc
+# ln -s $HOME/.dotfiles/.zshrc $HOME/.zshrc
 
 # Symlink the Mackup config file to the home directory
 ln -s $HOME/.dotfiles/.mackup.cfg $HOME/.mackup.cfg
